269cb55fd5c028725c35e461412fe37b1ca42987
root <root@hnet.ru>
2014-01-16 20:17:25 +0700

    Изменен shell, добавлен удаленный вызов методов

--- core/gcore_client/shell.php
+++ core/gcore_client/shell.php
@@ -12,7 +12,7 @@ $edx=null;
 print_r("Welcome to gcore shell\n");
 if(file_exists("config.ini"))
 {
-	print_r("<  Loading config file config.ini");
+	print_r("RX: Loading config file config.ini");
 	$tmp=parse_ini_file("config.ini");
 	$set=array_merge($set,$tmp);
 }
@@ -24,7 +24,7 @@ function done()
 while($cycle)
 {
 	try{
-		$welcome="\n>: ";
+		$welcome="\nTX: ";
 		if($mode=="php")
 			$welcome="";
 		$line=readline($welcome);
@@ -45,7 +45,7 @@ while($cycle)
 				$expr=preg_match("#php (.*)#usi",$line,$m);
 				if(empty($m[1]))
 				{
-					echo "<  Switching mode to inline-PHP\n";
+					echo "RX:  Switching mode to inline-PHP\n";
 					$mode="php";
 					break;
 				}
@@ -67,7 +67,8 @@ while($cycle)
 					$client->proxy=$set["proxy"];
 				if(isset($set["auth"]))
 					$client->auth=$set["auth"];
-				echo "<  Connected to ".$set["host"]."\n";
+				echo "RX: Open host ".$set["host"]."\n";
+				echo "RX: Connection established";
 				$connected=true;
 				break;
 			case "exec":
@@ -86,6 +87,9 @@ while($cycle)
 						$client->proxy=$set["proxy"];
 					if(isset($set["auth"]))
 						$client->auth=$set["auth"];
+					echo "RX: Open host ".$set["host"]."\n";
+					echo "RX: Connection established";
+					$connected=true;
 				}
 				if($set["useauth"])
 				{
@@ -95,18 +99,18 @@ while($cycle)
 				$res=$client->call($params,$set["debug"]);
 				if(!empty($e[1])&&strtolower($e[1])=="into"&&!empty($e[2])&&preg_match("#[a-z0-9]+#usi",$e[2]))
 				{
-					echo "<  Result stored into \$".$e[2]."\n";
+					echo "RX: Result stored into \$".$e[2]."\n";
 					${$e[2]}=$res;
 					break;
 				}
-				echo "<  Result stored into \$eax\n";
+				echo "RX: Result stored into \$eax\n";
 				$eax=$res;
 				print_r($res);
 				break;
 			case "print":
 				if(empty($e[1]))
 					throw new exception("you must specify what you're printing: config or data");
-				if(strtolower($e[1])=="config")
+				if(strtolower($e[1])=="config"||strtolower($e[1])=="cfg")
 				{
 					print_r($set);
 					break;
@@ -120,7 +124,7 @@ while($cycle)
 				{
 					if(isset(${$e[1]}))
 					{
-						print_r("<  ");
+						print_r("RX: ");
 						print_r(${$e[1]});
 						break;
 					}
@@ -129,7 +133,7 @@ while($cycle)
 			case "load":
 				if(empty($e[1]))
 					throw new exception("you must specify what you're loading: config or data path");
-				if(strtolower($e[1])=="config")
+				if(strtolower($e[1])=="config"||strtolower($e[1])=="cfg")
 				{
 					if(file_exists($e[2]))
 					{
@@ -160,7 +164,7 @@ while($cycle)
 					print chr(27)."[H".chr(27)."[2J";
 					break;
 				}
-				if(strtolower($e[1])=="config")
+				if(strtolower($e[1])=="config"||strtolower($e[1])=="cfg")
 				{
 					$set=array();
 					break;
@@ -172,11 +176,19 @@ while($cycle)
 				}
 				break;
 			case "set":
-				if(strtolower($e[1])=="config")
+				if(strtolower($e[1])=="config"||strtolower($e[1])=="cfg")
 				{
-					$e=explode("=",$e[2]);
-					$set[$e[0]]=$e[1];
+					preg_match("#^set ".strtolower($e[1])." (.*)#usi",$line,$m);
+					if(strlen(trim($m[1]))==0)
+						throw new exception("empty string");
+					$p=explode(",",$m[1]);
+					foreach($p as $i=>$sec)
+					{
+						$e=explode("=",trim($sec));
+						$set[$e[0]]=$e[1];
+					}
 					break;
+
 				}
 				$p=preg_match("#^set (.*)#usi",$line,$m);
 				if(strlen(trim($m[1]))==0)
@@ -191,10 +203,38 @@ while($cycle)
 			case "quit":
 				$cycle=false;
 				break;
+			case "close":
+				$connected=false;
+				print_r("RX:  Connection closed");
+				exit;
+			default:
+				if($connected)
+				{
+					if(preg_match("#^([a-z0-9_.]+)[.:]{1,2}([a-z0-9_.]+)\((.*)\)#usi",$line,$m))
+					{
+						$tmp=array();
+						if(strlen(trim($m[3]))!=0)
+						{
+							$p=explode(",",$m[3]);
+							foreach($p as $i=>$sec)
+							{
+								$e=explode("=",trim($sec));
+									$tmp[$e[0]]=$e[1];
+							}
+						}
+						$tmp["method"]=$m[1].".".$m[2];
+						$res=$client->call($tmp,$set["debug"]);
+						echo "RX:  Result stored into \$eax\n";
+						$eax=$res;
+						print_r($res);
+						break;
+					}
+				}
+				throw new exception("unknown command or connection closed");
 		}
 	}catch(exception $err){
-		print_r("<  Error\n");
-		print_r("<  #".$err->getCode().": ".$err->getMessage()."\n");
+		print_r("RX: Error\n");
+		print_r("RX: #".$err->getCode().": ".$err->getMessage()."\n");
 	}
 }
 if($connected)
