167cc96abccf505c8038381cdaa5fd53b7860e05
Evgeniy Vasiliev <johny@johnynsk.ru>
2014-04-17 15:08:20 +0700

    Изменена версия в core.php

--- core/core.php
+++ core/core.php
@@ -15,7 +15,7 @@ class core{
 	public $api_client=false;
 	public $api_auth=false;
 	static $api_version=0.9;
-	static $version="1.1.20140411";
+	static $version="1.1.20140416";
 	public function setLimit($params)
 	{
 		if(!isset($params["limit"])||$params["limit"]<=0)
